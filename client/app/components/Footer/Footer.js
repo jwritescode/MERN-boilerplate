d821014ee034c7ca89d32e71cb1081e45c45eb2a
johnwritescode <44175748+jwritescode@users.noreply.github.com>
2018-10-24 17:22:37 -0400

    Update client/app/components/Footer/Footer.js

--- client/app/components/Footer/Footer.js
+++ client/app/components/Footer/Footer.js
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import FlexView from 'react-flexview';
 const Footer = () => (
   <footer>
     <hr />
-    <p>johnwritescode 2018 - learning react, flexbox, mongodb, express and a handful of other technologies.</p>
+    <FlexView vAlignContent='center' hAlignContent='center'>johnwritescode 2018 - learning react, flexbox, mongodb, express and a handful of other technologies.</FlexView>
   </footer>
 );
 
