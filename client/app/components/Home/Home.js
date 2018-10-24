aac9a959a1972b0be7c2f1f5db54b24947ac2e69
johnwritescode <44175748+jwritescode@users.noreply.github.com>
2018-10-24 01:56:10 -0400

    Update client/app/components/Home/Home.js

--- client/app/components/Home/Home.js
+++ client/app/components/Home/Home.js
@@ -32,30 +32,30 @@ this.logout = this.logout.bind(this);
 
 //Functions for the sign up/sign in form
 componentDidMount() {
-  const obj = getFromStorage('the_main_app');
-  if (obj && obj.token) {
-    const { token } = obj;
-    //verify token
-    fetch('/api/account/verify?token=' + token)
-    .then(res => res.json())
-    .then(json => {
-      if (json.success) {
-      this.setState({
-        token,
-        isLoading: false,
-      });
-    } else {
-      this.setState({
-        isLoading: false,
-  });
-}
-});
-} else {
-  this.setState({
-    isLoading: false,
-  });
-}
-}
+   const obj = getFromStorage('the_main_app');
+   if (obj && obj.token) {
+     const { token } = obj;
+     // Verify token
+     fetch('/api/account/verify?token=' + token)
+       .then(res => res.json())
+       .then(json => {
+         if (json.success) {
+           this.setState({
+             token,
+             isLoading: false
+           });
+         } else {
+           this.setState({
+             isLoading: false,
+           });
+         }
+       });
+   } else {
+     this.setState({
+       isLoading: false,
+     });
+   }
+ }
 logout() {
     this.setState({
       isLoading: true,
