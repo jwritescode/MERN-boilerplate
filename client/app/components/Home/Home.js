186cfe69ba114ba80e15c8aa7cc15462cf5f4252
johnwritescode <44175748+jwritescode@users.noreply.github.com>
2018-10-24 01:36:47 -0400

    Update client/app/components/Home/Home.js

--- client/app/components/Home/Home.js
+++ client/app/components/Home/Home.js
@@ -24,6 +24,7 @@ this.onTextboxChangeSignInEmail = this.onTextboxChangeSignInEmail.bind(this);
 this.onTextboxChangeSignInPassword = this.onTextboxChangeSignInPassword.bind(this);
 this.onTextboxChangeSignUpEmail = this.onTextboxChangeSignUpEmail.bind(this);
 this.onTextboxChangeSignUpPassword = this.onTextboxChangeSignUpPassword.bind(this);
+
 this.onSignUp = this.onSignUp.bind(this);
 this.onSignIn = this.onSignIn.bind(this);
 this.logout = this.logout.bind(this);
